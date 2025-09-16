7b398a14b18b72d4ebaf621cc1f31d1a63b17d62
Lalith Kumar <lalithkumar27287@gmail.com>
2025-09-16 11:06:13 +0530

    Update palindrome_number.py

--- palindrome_number.py
+++ palindrome_number.py
@@ -1,4 +1,14 @@
+import sys
+
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        return str(x)[::-1]== str(x)
-        
+        return str(x)[::-1] == str(x)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python palindrome_check.py <integer>")
+    else:
+        num = int(sys.argv[1])
+        sol = Solution()
+        result = sol.isPalindrome(num)
+        print(f"{num} is a palindrome: {result}")
