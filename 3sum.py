079ebb7c08599c7ecf19c14337d6a2fc404db481
Lalith Kumar <lalithkumar27287@gmail.com>
2025-09-16 12:34:58 +0530

    Create 3sum.py

---
+++ 3sum.py
@@ -0,0 +1,36 @@
+import sys
+from typing import List
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        nums.sort()
+
+        for i in range(len(nums) - 2):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            l, r = i + 1, len(nums) - 1
+            while l < r:
+                total = nums[i] + nums[l] + nums[r]
+                if total < 0:
+                    l += 1
+                elif total > 0:
+                    r -= 1
+                else:
+                    res.append([nums[i], nums[l], nums[r]])
+                    while l < r and nums[l] == nums[l + 1]:
+                        l += 1
+                    while l < r and nums[r] == nums[r - 1]:
+                        r -= 1
+                    l += 1
+                    r -= 1
+        return res
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python three_sum.py <num1> <num2> ...")
+    else:
+        nums = list(map(int, sys.argv[1:]))
+        sol = Solution()
+        result = sol.threeSum(nums)
+        print("Triplets that sum to zero:", result)
